dc0fac7d59883b45b83b0a262386662e88a8ca78
estherkimyunjung <estherkimyunjung@gmail.com>
2021-03-22 20:45:33 -0500

    create main components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Chat from "./Chat";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
-      <h1>Start Build !!</h1>{" "}
+      <Sidebar />
+      <Chat />
     </div>
   );
 }
