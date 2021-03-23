da1ef481c812a76cd92f10a52e939d84aedb0d13
estherkimyunjung <estherkimyunjung@gmail.com>
2021-03-22 20:38:47 -0500

    add config for firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDdPA_OVk8WHkE5Oe3c4bmsaMCcriHBeEY",
+  authDomain: "livechat-mern.firebaseapp.com",
+  projectId: "livechat-mern",
+  storageBucket: "livechat-mern.appspot.com",
+  messagingSenderId: "466602922042",
+  appId: "1:466602922042:web:7d47439f51ea20f7016503",
+};
