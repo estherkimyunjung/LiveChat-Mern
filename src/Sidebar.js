7257f50c9823d9022bea531ee1f265d393e49f9d
estherkimyunjung <estherkimyunjung@gmail.com>
2021-04-02 00:40:53 -0500

    add search component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,6 +4,7 @@ import { Avatar, IconButton } from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
 
 function Sidebar() {
   return (
@@ -20,6 +21,12 @@ function Sidebar() {
           <IconButton>
             <MoreVertIcon />
           </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input type="text" placeholder="Search or start new chat" />
         </div>
       </div>
     </div>
