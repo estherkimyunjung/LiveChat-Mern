693121176ec4dba2d8ec495d79d9bc92f4fd10b2
estherkimyunjung <estherkimyunjung@gmail.com>
2021-04-02 00:26:53 -0500

    add sidebar header

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,27 @@
 import React from "react";
 import "./Sidebar.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>Sidebar Here</h1>
+      <div className="sidebar__header">
+        <Avatar src="https://avatars.githubusercontent.com/u/48709512?v=4" />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
